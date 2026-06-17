a48cef3325c9ceab527d975274779528ac9040cc
skyneem <sergey999q@yandex.ru>
2026-06-17 15:03:33 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
-    YaAuthSuggest.init(
+  window.YaAuthSuggest.init(
       {
         client_id: "138a60685aa94950933cab890897ac6b",
         response_type: "token",
-        redirect_uri: "​​​​​​​https://oauth-master-class-jet.vercel.app/token.html",
+        redirect_uri: "https://oauth-master-class-jet.vercel.app/token.html",
       },
       "https://oauth-master-class-jet.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
     )
-   .then(({ handler }) => handler())
+      .then(({ handler }) => handler())
       .then(async (data) => {
         const result = await fetchYandexData(data.access_token);
 
@@ -17,5 +24,5 @@ window.onload = () => {
         console.log(result, data);
       })
       .catch((error) => console.log("Что-то пошло не так: ", error));
-  };
+};
 };
